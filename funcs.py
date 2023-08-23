54fcd96f30547a0fe4e1e97c3b188cdecde854e9
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-08-23 14:27:12 +0500

    add new funcs

--- funcs.py
+++ funcs.py
@@ -1,3 +1,9 @@
 def sum_num(num_one, num_two):
-    sum=num_one +num_two
+    sum = num_one + num_two
     return sum
+
+
+def sub_num(num_one, num_two):
+    sum = num_one + num_two
+    return sum
+
